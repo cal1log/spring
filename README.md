56f98ebba64e3b41be9435f72b57377099b13817
oran2527 <orlago250183@gmail.com>
2021-07-16 11:11:35 -0500

    lesson 1-2-3-4

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # spring
 
 [spring](https://www.youtube.com/watch?v=aZlGNXtGbr8&list=PLK0V_H0fCvPgityADYcP_IbdNeqp3zCXr)
+
+# lessons
+
+## 16 july 2021 - lesson 1-2-3-4
---
+++ projapp/src/test/java/com/cal1log/projapp/ProjappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal1log.projapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
